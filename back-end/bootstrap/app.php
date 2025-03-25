8633d99ab99f10a1954e9fc92df00bb5dc0338be
sahnoun122 <khadijasahnoun70@gmail.com>
2025-03-25 12:50:41 +0000

    ajouter le middelware jwtmiddelware dans app.php

--- back-end/bootstrap/app.php
+++ back-end/bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\AuthMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,13 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-
-        // $middleware->append(AuthMiddleware::class);
+        $middleware->alias([
+            'jwt.auth' => \App\Http\Middleware\JwtMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
-
-$app->routeMiddleware([
-    'auth.middleware' => AuthMiddleware::class,
-]);
