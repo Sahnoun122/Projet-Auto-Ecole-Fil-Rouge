5ba7f07c973ab231b5712d5cd40ac410b0a6ce58
sahnoun122 <khadijasahnoun70@gmail.com>
2025-03-25 11:43:47 +0000

    ajouter authRepository

---
+++ back-end/Repositories/AuthRepository.php
@@ -0,0 +1,26 @@
+<?php
+// app/Repositories/UserRepository.php
+namespace App\Repositories;
+
+use App\Models\User;
+
+class AuthRepository implements AuthRepositoryInterface
+
+
+{
+    public function register( $data)
+    {
+        return User::create($data);
+    }
+
+    public function Connecter( $email)
+    {
+        return User::where('email', $email)->first();
+    }
+
+    public function modifierMotDePasse($user, $password)
+    {
+        $user->password = bcrypt($password);
+        $user->save();
+    }
+}
