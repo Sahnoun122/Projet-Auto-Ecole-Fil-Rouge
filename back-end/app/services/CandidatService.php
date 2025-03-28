4e7837981eb9e57a1b6944351d6cd6eb5aa9ad09
sahnoun122 <khadijasahnoun70@gmail.com>
2025-03-28 14:32:07 +0000

    la creation de candidatservices

---
+++ back-end/app/services/CandidatService.php
@@ -0,0 +1,43 @@
+<?php 
+
+// app/Services/CandidatService.php
+namespace App\Services;
+
+use App\Repositories\CandidatRepository;
+use Illuminate\Support\Facades\Storage;
+
+class CandidatService
+{
+    protected $candidatRepository;
+
+    public function __construct(CandidatRepository $candidatRepository)
+    {
+        $this->candidatRepository = $candidatRepository;
+    }
+
+    public function createOrUpdateCandidat($userId, array $data)
+    {
+        $candidatData = [
+            'type_permis' => $data['type_permis'],
+            'id_candidat' => $userId
+        ];
+
+        if (isset($data['photo_identite'])) {
+            $photoPath = $data['photo_identite']->store('identite', 'public');
+            $candidatData['photo_identite'] = $photoPath;
+        }
+
+        $existingCandidat = $this->candidatRepository->getCandidat($userId);
+        
+        if ($existingCandidat) {
+            return $this->candidatRepository->updateCandidat($userId, $candidatData);
+        }
+
+        return $this->candidatRepository->completeCandidat($candidatData);
+    }
+
+    public function getCandidatInfo($userId)
+    {
+        return $this->candidatRepository->getCandidat($userId);
+    }
+}
