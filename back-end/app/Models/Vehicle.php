f97cadf74f9ec6d33ea0eb19b96b59e070d74312
sahnoun122 <khadijasahnoun70@gmail.com>
2025-04-03 19:46:48 +0000

    la creation de model de vehicule

---
+++ back-end/app/Models/Vehicle.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Vehicle extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'marque', 'modele', 'immatriculation', 'date_achat',
+        'kilometrage', 'prochaine_maintenance', 'statut', 'admin_id'
+    ];
+
+    protected $casts = [
+        'date_achat' => 'date',
+        'prochaine_maintenance' => 'date'
+    ];
+
+    public function admin(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'admin_id');
+    }
+}
