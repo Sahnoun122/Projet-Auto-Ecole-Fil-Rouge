9c80bc1299ce70cd229d6f89eb435f04a72ce400
sahnoun122 <khadijasahnoun70@gmail.com>
2025-04-04 18:44:27 +0000

    la creation de controller profile rt ajouter la methodes show profile

---
+++ back-end/app/Http/Controllers/API/ProfileController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+
+class ProfileController extends Controller
+{
+    public function show()
+    {
+        $user = Auth::user();
+        return response()->json([
+            'user' => $user,
+            'photo_profile_url' => $user->profile_photo_url,
+        ]);
+    }
+
+}
