7328d369e848963809e41666333d3de59d811ac4
sahnoun122 <khadijasahnoun70@gmail.com>
2025-04-17 23:43:22 +0100

    la creations de la migrations exam_candidats

---
+++ back-end/database/migrations/2025_04_17_224021_create_exam_candidat_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('exam_candidat', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('exam_id')->constrained()->onDelete('cascade');
+            $table->foreignId('candidat_id')->constrained('users')->onDelete('cascade');
+            $table->boolean('present')->default(false);
+            $table->enum('resultat', ['excellent', 'tres_bien', 'bien', 'moyen', 'insuffisant'])->nullable();
+            $table->integer('score')->nullable();
+            $table->text('observations')->nullable();
+            $table->text('feedbacks')->nullable();
+            $table->timestamps();
+
+            $table->unique(['exam_id', 'candidat_id']);
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('exam_candidat');
+    }
+};
